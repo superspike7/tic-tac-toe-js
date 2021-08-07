fb24d0472f6a8ec1995ea7cbabf40385b43ff6ad
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-07 09:18:53 +0800

    add check winner method

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const gameBoard = ( () => {
 
   const getSquare = (pos) => { return board[pos] };
 
-  const getLine = (a,b,c) => {
-    return [board[a], board[b], board[c]];
+  const getLine = (arr) => {
+    return [board[arr[0]], board[arr[1]], board[arr[2]] ];
   };
 
   return {board, setSquare, getSquare, getLine};
@@ -24,7 +24,22 @@ const Player = (name, piece) => {
 
 };
 
-const gameController = () => {
-  const winnerPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,8], [2,5,8], [0,4,8], [2,4,6]];
-};
+const gameController = ( () => {
+  const winningPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,8], [2,5,8], [0,4,8], [2,4,6]];
+
+  const checkWin = () => {
+   let winner = false;
+   winningPatterns.forEach(pattern => {
+    let line = gameBoard.getLine(pattern).join("");
+    if (line === "XXX") {
+      winner = "X";
+    } else if (line === "OOO") {
+      winner = "O";
+    }
+   });
+   return winner;
+  };
+
+  return {checkWin};
+})();
 
