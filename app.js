2c457835bbe48a88b06554bc5d5f554520c65d30
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-08 17:42:53 +0800

    fix middle vertical winning pattern bug

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const Player = (name, piece) => {
 };
 
 const gameController = ( () => {
-  const winningPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,8], [2,5,8], [0,4,8], [2,4,6]];
+  const winningPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
   let playerNow = "X";
 
 
